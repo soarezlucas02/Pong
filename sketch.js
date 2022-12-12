4126f4fe2edc12345b0343e82c9819dd713ed250
soarezlucas02 <lucassoaresesouza@gmail.com>
2022-12-11 21:16:05 -0300

    Diminuindo a Velocidade da Bolinha

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ let xBolinha=300;
 let yBolinha=200;
 let diametro=20;
 
-let velocidadebX=10;
-let velocidadebY=10;
+let velocidadebX=5;
+let velocidadebY=5;
 
 //variaveis da raquete
 let raqueteComprimento=10;
